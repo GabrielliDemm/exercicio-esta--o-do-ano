0e466d3f9cfa1011d0d491797f86ae7a02da0703
Gabriellidemm <felixgabrielli768@gmail.com>
2025-05-09 11:23:00 -0300

    commit melhorias Estações do ano

---
+++ EstaçõesDoAno.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class EstaçõesDoAno {
+
+    public static void main(String[] args) {
+        //Declaração de variaveis
+        int mes, dia;
+
+        //Criação e instancia do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        //Apresentção
+        System.out.println("\n\t\t\t -- Estações do Ano --\n");
+
+        //Entrada
+        System.out.print("Informe o dia: ");
+        dia = entrada.nextInt();
+        System.out.println("Imforme o mês");
+        mes = entrada.nextInt();
+
+
+        //processamento
+        if ((mes == 1) || (mes == 2) || (mes == 3)){
+            System.out.println("É verão!");
+        } else if ((mes == 4) || (mes == 5) || (mes == 6)){
+            System.out.println("É Outono!");
+        }else if ((mes == 7) || (mes ==8) || (mes == 9)){
+            System.out.println("É Inverno<3");
+        }else if ((mes == 10) || (mes == 11)|| (mes == 12)){
+            System.out.println("É Primavera!");
+        
+        }else
+
+        //não é mês
+        System.out.println("mês" + mes + "não existe >:(");
+
+
+    }
+}
