7ca5f5809fb9212afbfd8c71f17cc266d2234104
Gabriellidemm <felixgabrielli768@gmail.com>
2025-05-09 11:47:51 -0300

    commit switch case

--- EstaçõesDoAno.java
+++ EstaçõesDoAno.java
@@ -20,36 +20,51 @@ public class EstaçõesDoAno {
 
 
         //processamento
-        if ((mes == 1) || (mes == 2) || (mes == 3)){
-            if((mes == 3) && (dia >= 20)){
-                System.out.println("É outono");
-            }else {
-                System.out.println("É verão!");
-            }
-        } else if ((mes == 4) || (mes == 5) || (mes == 6)){
-            if((mes == 6) && (dia >= 20)){
-            System.out.println("É Inverno<3");
-            }else {
-            System.out.println("É Outono!");
-            }
-        }else if ((mes == 7) || (mes ==8) || (mes == 9)){
-            if((mes == 9) && (dia >= 22)){
-                System.out.println("É Primavera!");
-            }else {
-            System.out.println("É Inverno<3");
-            }
-        }else if ((mes == 10) || (mes == 11)|| (mes == 12)){
-            if((mes == 12) && (dia >= 21)){
-                System.out.println("É verão!");
-            }else {
-                System.out.println("É Primavera!");
-            }
-        
-        }else
-
-        //não é mês
-        System.out.println("mês" + mes + "não existe >:(");
-
+        switch (mes) {
+            case 1:
+            case 2:
+            case 3:
+                //saida
+                if((mes == 3) && (dia >= 20)){
+                    System.out.println("É outono");
+                }else {
+                    System.out.println("É verão!");
+                }
+                break;
+            case 4:
+            case 5:
+            case 6:
+                //saida
+                if((mes == 6) && (dia >= 20)){
+                System.out.println("É Inverno<3");
+                }else {
+                System.out.println("É Outono!");
+                }
+                break;
+            case 7:
+            case 8:
+            case 9:
+                //saida
+                if((mes == 9) && (dia >= 22)){
+                    System.out.println("É Primavera!");
+                }else {
+                System.out.println("É Inverno<3");
+                }
+                break;
+            case 10:
+            case 11:
+            case 12:
+                //saida
+                if((mes == 12) && (dia >= 21)){
+                    System.out.println("É verão!");
+                }else {
+                    System.out.println("É Primavera!");
+                }
+                break;
+            default:
+                //saida
+                System.out.println("mês" + mes + "não existe >:(");
+        }
 
     }
 }
