4667572ff33c307b05a4ca14d8744d0e6cff15ab
Gabriellidemm <felixgabrielli768@gmail.com>
2025-05-09 11:26:29 -0300

    commit melhorias estações do ano

--- EstaçõesDoAno.java
+++ EstaçõesDoAno.java
@@ -21,13 +21,29 @@ public class EstaçõesDoAno {
 
         //processamento
         if ((mes == 1) || (mes == 2) || (mes == 3)){
-            System.out.println("É verão!");
+            if((mes == 3) && (dia >= 20)){
+                System.out.println("É outono");
+            }else {
+                System.out.println("É verão!");
+            }
         } else if ((mes == 4) || (mes == 5) || (mes == 6)){
-            System.out.println("É Outono!");
-        }else if ((mes == 7) || (mes ==8) || (mes == 9)){
+            if((mes == 6) && (dia >= 20)){
             System.out.println("É Inverno<3");
+            }else {
+            System.out.println("É Outono!");
+            }
+        }else if ((mes == 7) || (mes ==8) || (mes == 9)){
+            if((mes == 9) && (dia >= 22)){
+                System.out.println("É Primavera!");
+            }else {
+            System.out.println("É Inverno<3");
+            }
         }else if ((mes == 10) || (mes == 11)|| (mes == 12)){
-            System.out.println("É Primavera!");
+            if((mes == 12) && (dia >= 21)){
+                System.out.println("É verão!");
+            }else {
+                System.out.println("É Primavera!");
+            }
         
         }else
 
